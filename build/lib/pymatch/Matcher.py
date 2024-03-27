692da1ecf73b1d7877331a767f5f0a8509d235c3
John Kohler - j0k0ccf <John.Kohler@walmart.com>
2024-03-27 10:50:16 -0400

    print in plot func

--- build/lib/pymatch/Matcher.py
+++ build/lib/pymatch/Matcher.py
@@ -300,6 +300,7 @@ class Matcher:
                                            std_diff_med_after, std_diff_mean_after))
             ax2.legend(loc="lower right")
             plt.xlim((0, np.percentile(xta.x, 99)))
+            print(col)
             plt.show()
 
             return {
